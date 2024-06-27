679078578b6a1cd933c4ad455416e59e722e4479
ekaktusz <csaba.ekart@gmail.com>
2024-06-27 14:36:56 +0200

    compilation in wsl

--- build.sh
+++ build.sh
@@ -1,10 +1,21 @@
+#!/bin/bash
+
+# Set environment variables
+export VCPKG_MAX_CONCURRENCY=8
+export VCPKG_BINARY_SOURCES="clear;files,$HOME/.vcpkg-cache,readwrite"
+
 if [ $# -eq 0 ]; then
-    cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_EXPORT_COMPILE_COMMANDS=1 -H. -Bout/build
-    make -j 8 -C out/build
+    cmake -DCMAKE_BUILD_TYPE=Debug \
+          -DCMAKE_EXPORT_COMPILE_COMMANDS=1 \
+          -DCMAKE_CXX_COMPILER_LAUNCHER=ccache \
+          -H. -Bout/build -G Ninja
+    ninja -C out/build
+    mkdir -p .vscode
+    cp out/build/compile_commands.json ./compile_commands.json
 fi
 
 if [[ $1 == "clean" ]]; then
     cmake --build out/build --target clean
-    rm -r out/*
+    rm -rf out/*
     echo "Project cleaned."
 fi
