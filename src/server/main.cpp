bbcdc8ce6a96de7ee6595b2d34df384430175ee4
Ekart Csaba <csaba.ekart@gmail.com>
2025-06-23 19:49:37 +0200

    add server target to cmake

---
+++ src/server/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main()
+{
+	std::cout << "Hello, World!" << std::endl;
+	// This is a placeholder for the main function of your server application.
+	// You can add your server initialization and running logic here.
+	return 0;
+}
